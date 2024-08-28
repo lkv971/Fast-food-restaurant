9f64fec564bd68823635bcc7d604d09ada2bbcd2
lkv971 <leeroyvaillant@outlook.com>
2024-08-28 13:21:13 -0400

    Create insert_data.sql

---
+++ rdbms scripts/insert_data.sql
@@ -0,0 +1,134 @@
+Use FastFoodDB;
+GO
+
+BULK INSERT Ingredients 
+FROM "C:\Users\ACER\Documents\GitHub\Fast-food-restaurant\raw data\ingredients.csv"
+WITH (
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+ROWTERMINATOR = '/n',
+TABLOCK,
+FORMAT = 'CSV'
+)
+;
+GO
+
+BULK INSERT MenuItems
+FROM "C:\Users\ACER\Documents\GitHub\Fast-food-restaurant\raw data\menu_items.csv"
+WITH (
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+ROWTERMINATOR = '/n',
+TABLOCK,
+FORMAT = 'CSV'
+)
+;
+GO
+
+BULK INSERT MenuItems
+FROM "C:\Users\ACER\Documents\GitHub\Fast-food-restaurant\raw data\menu_items.csv"
+WITH (
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+ROWTERMINATOR = '/n',
+TABLOCK,
+FORMAT = 'CSV'
+)
+;
+GO
+
+BULK INSERT SalesDetails
+FROM "C:\Users\ACER\Documents\GitHub\Fast-food-restaurant\raw data\menuitem.csv"
+WITH (
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+ROWTERMINATOR = '/n',
+TABLOCK,
+FORMAT = 'CSV'
+)
+;
+GO
+
+BULK INSERT PortionTypes
+FROM "C:\Users\ACER\Documents\GitHub\Fast-food-restaurant\raw data\portion_uom_types.csv"
+WITH (
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+ROWTERMINATOR = '/n',
+TABLOCK,
+FORMAT = 'CSV'
+)
+;
+GO
+
+BULK INSERT OrderTransactioins
+FROM "C:\Users\ACER\Documents\GitHub\Fast-food-restaurant\raw data\pos_ordersale.csv"
+WITH (
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+ROWTERMINATOR = '/n',
+TABLOCK,
+FORMAT = 'CSV'
+)
+;
+GO
+
+BULK INSERT Recipes
+FROM "C:\Users\ACER\Documents\GitHub\Fast-food-restaurant\raw data\recipes.csv"
+WITH (
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+ROWTERMINATOR = '/n',
+TABLOCK,
+FORMAT = 'CSV'
+)
+;
+GO
+
+BULK INSERT Stores
+FROM "C:\Users\ACER\Documents\GitHub\Fast-food-restaurant\raw data\store_restaurant.csv"
+WITH (
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+ROWTERMINATOR = '/n',
+TABLOCK,
+FORMAT = 'CSV'
+)
+;
+GO
+
+BULK INSERT Stores
+FROM "C:\Users\ACER\Documents\GitHub\Fast-food-restaurant\raw data\store_restaurant.csv"
+WITH (
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+ROWTERMINATOR = '/n',
+TABLOCK,
+FORMAT = 'CSV'
+)
+;
+GO
+
+BULK INSERT SubRecipeIngredientAssignments
+FROM "C:\Users\ACER\Documents\GitHub\Fast-food-restaurant\raw data\sub_recipe_ingr_assignments.csv"
+WITH (
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+ROWTERMINATOR = '/n',
+TABLOCK,
+FORMAT = 'CSV'
+)
+;
+GO
+
+BULK INSERT SubRecipes
+FROM "C:\Users\ACER\Documents\GitHub\Fast-food-restaurant\raw data\sub_recipes.csv"
+WITH (
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+ROWTERMINATOR = '/n',
+TABLOCK,
+FORMAT = 'CSV'
+)
+;
+GO
